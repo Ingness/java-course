68556e7324267659e5a64430d8e6691672e74694
Ingness <olga.tobolska@gmail.com>
2020-04-07 16:01:38 +0200

    First block

---
+++ src/main/java/pl/wsb/students/api/consts/UserResource.java
@@ -0,0 +1,20 @@
+package pl.wsb.students.api.consts;
+
+import pl.wsb.students.api.model.RegisterUserRequest;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path(ApiEndpoints.USER)
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class UserResource {
+    @POST
+    public Response postUser(RegisterUserRequest body) {
+        return Response.status(Response.Status.OK).entity("mock call ok...").build();
+    }
+}
